1e6955cc199d4bcaabad2b90f3b35849fa05e66b
Krus <kattykitty176@gmail.com>
2024-10-09 19:40:49 +0800

    refractor: modified colors of navbar via main.css

--- app/Views/landing.php
+++ app/Views/landing.php
@@ -3,6 +3,14 @@
 <div>
     <h1>Hello</h1>
 </div>
-section.hero>(.left>(h3>lorem)+(p>lorem50))+.right>((h4>lorem5)+(p>lorem100))
-section.hero>(.left>(h3>lorem)+(p>lorem50))+.right>((h4>lorem5)+(p>lorem100))
+<section class="hero"></section>
+    <div class="left">
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, quam tempora? Enim eligendi recusandae sit voluptate tempore, consequuntur quis ipsa alias, facilis fugiat consequatur quas rem aspernatur ipsum in qui?</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ut distinctio voluptatum at, nesciunt harum beatae rem dicta, cum enim odio temporibus. Itaque unde officiis cum id maiores optio iste esse quidem maxime, laboriosam impedit delectus iure modi repellat, pariatur sed velit voluptates laudantium a corporis ad assumenda rerum placeat.</p>
+    </div>
+    <div class="right">
+        <h4>Lorem ipsum dolor sit amet.</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id qui sapiente atque assumenda quasi ratione, itaque ducimus inventore nemo, laudantium illo, quidem neque ab. Quam a magnam quidem! Cupiditate pariatur doloribus similique saepe accusamus, nemo temporibus, eos incidunt corrupti ullam praesentium necessitatibus, consectetur quia vitae! Veritatis incidunt ut ratione atque dicta nam, eaque voluptates ab aspernatur deserunt dolore facilis facere deleniti ex ipsam, dignissimos debitis. Labore enim in modi optio corrupti molestias, laudantium nesciunt voluptas saepe? Tenetur obcaecati maiores, unde quam molestias velit fugiat aut deserunt minima blanditiis quae porro fuga cum quasi in! At dolor vel nostrum eum consequatur.</p>
+    </div>
+</section>
 <?= $this->endSection('contents') ?>
